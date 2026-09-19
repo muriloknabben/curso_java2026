239296523ff21624ca415af185f2e5654630a70a
muriloknabben <murilofknabben@gmail.com>
2026-09-19 00:03:19 -0300

    estrtura condicional

---
+++ ac_estrutura_condicional/a_estrutura_condicional/src/main/java/com/ec/app/Main.java
@@ -0,0 +1,30 @@
+package main.java.com.ec.app;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int hora;
+
+        System.out.println("Que horas sao?");
+        hora = sc.nextInt();
+        
+        if (hora < 12) {
+            System.out.println("Bom dia!");
+        }
+
+        else if (hora < 18) {
+            System.out.println("Bom tarde!");
+        }
+
+        else {
+            System.out.println("Boa noite!");
+        }
+        
+        sc.close();
+    }
+}
---
+++ ac_estrutura_condicional/b_exercicio3/src/main/java/com/ex3/app/Main.java
@@ -0,0 +1,145 @@
+package main.java.com.ex3.app;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+
+        int N1 = sc.nextInt();
+	    
+		if (N1 < 0) {
+			System.out.println("NEGATIVO");
+		}
+		else {
+			System.out.println("NAO NEGATIVO");
+		}
+
+        //Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+        int N2 = sc.nextInt();
+	    
+		if (N2 % 2 == 0) {
+			System.out.println("PAR");
+		}
+		else {
+			System.out.println("IMPAR");
+		}
+
+        //Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+        //Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+        //ordem crescente ou decrescente.
+
+        int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if (A % B == 0 || B % A == 0) {
+			System.out.println("Sao Multiplos");
+		}
+		else {
+			System.out.println("Nao sao Multiplos");
+		}
+
+        //Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode
+        //começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+
+        int horaInicial = sc.nextInt();
+		int horaFinal = sc.nextInt();
+		
+		int duracao;
+		if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+		}
+		else {
+			duracao = 24 - horaInicial + horaFinal;
+		}
+
+        System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+
+        //Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
+        //seguir, calcule e mostre o valor da conta a pagar.
+
+        int codigo = sc.nextInt();
+		int quantidade = sc.nextInt();
+		
+		double total;
+		if (codigo == 1) {
+			total = quantidade * 4.0;
+		}
+		else if (codigo == 2) {
+			total = quantidade * 4.5;
+		}
+		else if (codigo == 3) {
+			total = quantidade * 5.0;
+		}
+		else if (codigo == 4) {
+			total = quantidade * 2.0;
+		}
+		else {
+			total = quantidade * 1.5;
+		}
+
+		System.out.printf("Total: R$ %.2f%n", total);
+
+        //Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
+        //seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
+        //nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+
+        double numero = sc.nextDouble();
+		
+		if (numero < 0.0 || numero > 100.0) {
+			System.out.println("Fora de intervalo");
+		}
+		else if (numero <= 25.0) {
+			System.out.println("Intervalo [0,25]");
+		}
+		else if (numero <= 50.0) {
+			System.out.println("Intervalo (25,50]");
+		}
+		else if (numero <= 75.0) {
+			System.out.println("Intervalo (50,75]");
+		}		
+		else {
+			System.out.println("Intervalo (75,100]");
+		}
+
+        //Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+        //de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
+        //ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+        //Se o ponto estiver na origem, escreva a mensagem “Origem”.
+        //Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a
+        //situação.
+
+        double x = sc.nextDouble();
+		double y = sc.nextDouble();
+		
+		if (x == 0.0 && y == 0.0) {
+			System.out.println("Origem");
+		}
+		else if (x == 0.0) {
+			System.out.println("Eixo Y");
+		}
+		else if (y == 0.0) {
+			System.out.println("Eixo X");
+		}
+		else if (x > 0.0 && y > 0.0) {
+			System.out.println("Q1");
+		}
+		else if (x < 0.0 && y > 0.0) {
+			System.out.println("Q2");
+		}
+		else if (x < 0.0 && y < 0.0) {
+			System.out.println("Q3");
+		}
+		else {
+			System.out.println("Q4");
+		}
+
+        sc.close();
+    }
+    
+}
