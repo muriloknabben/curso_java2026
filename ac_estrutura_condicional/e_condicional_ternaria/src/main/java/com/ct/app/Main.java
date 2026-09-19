dd7689e1ae04ef5099c80291f76f33fe25815f75
muriloknabben <murilofknabben@gmail.com>
2026-09-19 09:51:05 -0300

    condicao ternaria

---
+++ ac_estrutura_condicional/e_condicional_ternaria/src/main/java/com/ct/app/Main.java
@@ -0,0 +1,13 @@
+package main.java.com.ct.app;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        double preco = 34.5;
+        
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println(desconto);
+    }
+}
