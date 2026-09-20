ca8ad862db949ab0ffde7b598d56db53fc7bc7d6
muriloknabben <murilofknabben@gmail.com>
2026-09-20 11:25:12 -0300

    funcoes(sintaxe)

---
+++ ae.outros_topicos_basicos/c_funcoes(sintaxe)/src/main/java/com/fun/app/Main.java
@@ -0,0 +1,42 @@
+package main.java.com.fun.app;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int higher = max(a, b, c);
+
+        showResult(higher);
+
+        sc.close();
+
+    }
+
+    public static int max(int x, int y, int z) {
+
+        if (x > y && x > z) {
+            return x;
+        } else if (y > z) {
+            return y;
+        } else {
+            return z;
+        }
+
+    }
+
+    public static void showResult(int value) {
+
+        System.out.println("Higher = " + value);
+
+    }
+
+}
