a7d6482b5716f4d19ce2d017da1a6f9894bed083
muriloknabben <murilofknabben@gmail.com>
2026-09-22 08:56:45 -0300

    encapasulamento

---
+++ ag_construtores_this_sobrecarga_encapsulamento/c_encapsulamento/src/app/Main.java
@@ -0,0 +1,52 @@
+package app;
+
+import java.util.Scanner;
+
+import model.Product;
+
+public class Main {
+
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+
+        Product product = new Product(name, price);
+
+        System.out.println();
+
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+
+        product.addProducts(quantity);
+
+        System.out.println();
+
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+
+        product.removeProducts(quantity);
+
+        System.out.println();
+
+        System.out.println("Updated data: " + product);
+
+        sc.close();
+
+    }
+}
