70e6041f1d75ca330a0ceaf5e0bd34eb9c173fba
muriloknabben <murilofknabben@gmail.com>
2026-09-25 08:22:21 -0300

    boxing unboxing e wrapper

---
+++ ah_arrays_listas/c_boxing_unboxing_wrapper/src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 20;
+
+        Object obj = x;
+
+        System.out.println(obj);
+
+        int y = (int) obj * 2;
+
+        System.out.println(y);
+
+    }
+}
